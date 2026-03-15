93a9477a92f2cbe4ae69b947f5565e6f48750659
Matheus Sousa <matheusgomessousa072@gmail.com>
2026-03-14 21:21:07 -0300

    otimização de codigo

--- desafio.c
+++ desafio.c
@@ -3,6 +3,22 @@
 
 #define btnA 5
 const uint RGB[] = {11, 12, 13};
+const bool colors[8][3] = {
+    {0, 0, 0}, //desligado
+    {0, 0, 1}, //vermelho
+    {0, 1, 0}, //azul
+    {1, 0, 0}, //verde
+    {1, 1, 0}, //ciano
+    {1, 0, 1}, //amarelo
+    {0, 1, 1}, //magenta
+    {1, 1, 1}, //branco
+}; 
+
+void set_rgb(int passo){
+    for(int i; i < 3; i++){
+        gpio_put(RGB[i], colors[passo][i]);
+    }
+}
 int main()
 {
     stdio_init_all();
@@ -18,67 +34,11 @@ int main()
 
     while(true){
         if(gpio_get(btnA)){
-            passo += 1;
-            if(passo > 7){
-                passo = 0;
-            }
+            passo = (passo + 1) % 8;
             while(gpio_get(btnA)){
                 sleep_ms(50);
             }
         }
-
-        switch (passo)
-        {
-        case 1:
-        // vermelho
-            gpio_put(RGB[0], 0);
-            gpio_put(RGB[1], 0);
-            gpio_put(RGB[2], 1);
-            break;
-        case 2:
-        // verde
-            gpio_put(RGB[0], 1);
-            gpio_put(RGB[1], 0);
-            gpio_put(RGB[2], 0);
-            break;
-        case 3:
-        // azul
-            gpio_put(RGB[0], 0);
-            gpio_put(RGB[1], 1);
-            gpio_put(RGB[2], 0);
-            break;
-        case 4:
-        // amarelo
-            gpio_put(RGB[0], 1);
-            gpio_put(RGB[1], 0);
-            gpio_put(RGB[2], 1);
-            break;
-        case 5:
-        // ciano
-            gpio_put(RGB[0], 1);
-            gpio_put(RGB[1], 1);
-            gpio_put(RGB[2], 0);
-            break;
-        case 6:
-        // magenta
-            gpio_put(RGB[0], 0);
-            gpio_put(RGB[1], 1);
-            gpio_put(RGB[2], 1);
-            break;
-        case 7:
-        // branco
-            gpio_put(RGB[0], 1);
-            gpio_put(RGB[1], 1);
-            gpio_put(RGB[2], 1);
-            break;
-        
-        default:
-            gpio_put(RGB[0], 0);
-            gpio_put(RGB[1], 0);
-            gpio_put(RGB[2], 0);
-            break;
-        }
+        set_rgb(passo);
     }
-
-   
-}
+    }
